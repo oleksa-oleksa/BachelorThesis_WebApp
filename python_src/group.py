262ecfa0dcfc1e658145dc7cca8d8ac05ae82e08
Oleksandra Baga <oleksandra.baga@gmail.com>
2019-10-06 10:30:29 +0200

    docstrings added to the classes

--- python_src/group.py
+++ python_src/group.py
@@ -1,2 +1,5 @@
 class Group:
+    """
+    Represents the group of 2 student, the branch (A/B) should be assigned
+    """
     pass
--- python_src/loan_journal.py
+++ python_src/loan_journal.py
@@ -1,2 +1,5 @@
 class LoanJournal:
+    """
+    Holds the record about the loan operation, student, board and time
+    """
     pass
