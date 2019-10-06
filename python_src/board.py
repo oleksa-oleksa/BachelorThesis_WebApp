a8763b3d5d1b98ea4a0c392a544609bb9b3e39f1
Oleksandra Baga <oleksandra.baga@gmail.com>
2019-10-06 10:30:29 +0200

    docstrings added to the classes

--- python_src/board.py
+++ python_src/board.py
@@ -1,3 +1,9 @@
 class Board:
+    """
+    Represents the Raspberry Pi board at university laboratory
+    There are 15 boards and every board has a number and a RFID Tag on it.
+    The boards numbered 0-10 should be used in the laboratory during the exercise lesson.
+    The boards numbered 11-15 could be loaned by authorised student for home usage.
+    """
     pass
 
--- python_src/student.py
+++ python_src/student.py
@@ -1,3 +1,12 @@
 class Student:
+    """
+    Represents a student in database. The personal date will be specified
+    by the admin after student has made enrollment to the course.
+    The group will be specified on the first lecture by the students/teaching assistance
+    home_loan_enabled flag is to set by the admin in case the board was returned damaged
+    and a student has not notified the teaching assistance about what happened
+    list_of_board can contain maximum two boards: one loaned to work in the laboratory during exercise lesson
+    and one taken for home_loan.
+    """
     pass
 
