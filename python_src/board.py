edf8a2992936c118e82a3f849274e0af325906a8
Oleksandra Baga <oleksandra.baga@gmail.com>
2019-10-10 08:29:58 +0200

    board init

--- python_src/board.py
+++ python_src/board.py
@@ -5,7 +5,15 @@ class Board:
     The boards numbered 0-10 should be used in the laboratory during the exercise lesson.
     The boards numbered 11-15 could be loaned by authorised student for home usage.
     """
-    def __init__(self, board_no=0, board_rfid_tag=None, loan_type=None):
+    def __init__(self, board_no=0, board_rfid_tag=None, board_type=None, board_status='Active',
+                 is_board_loaned=False, loan_date=None, student=None):
         """Creates a new board with given parameters"""
+        self.board_no = board_no
+        self.board_rfid_tag = board_rfid_tag
+        self.board_type = board_type
+        self.board_status = board_status
+        self.is_board_loaned = is_board_loaned
+        self.loan_date = loan_date
+        self.student = student
 
 
