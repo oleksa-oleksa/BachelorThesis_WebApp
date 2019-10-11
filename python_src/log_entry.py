f3c61db9cb7f265a564d59fdbf1ea9c3cb4a5160
Oleksandra Baga <oleksandra.baga@gmail.com>
2019-10-11 20:57:56 +0200

    log_entry init

--- python_src/log_entry.py
+++ python_src/log_entry.py
@@ -1,5 +1,14 @@
+from datetime import datetime
+from .operation import Operation
+
 class LogEntry:
     """
     Holds the record about the loan operation, student, board and time
     """
-    pass
+    def __init__(self, student=None, board=None, timestamp=None, operation=6):
+        """A single entry about board loan/return operation"""
+        self.student = student
+        self.board = board
+        self.timestamp = timestamp
+        self.operation = Operation(operation)
+
