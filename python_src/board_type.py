a5990bac3df4f0de5cf90eb4181c318fd8702f05
Oleksandra Baga <oleksandra.baga@gmail.com>
2020-01-27 12:34:58 +0100

    changed variables name to PEP8

--- python_src/board_type.py
+++ python_src/board_type.py
@@ -3,5 +3,5 @@ from enum import Enum
 
 class BoardType(Enum):
     """Represents two types of boards: for laboratory usage only and for home loan only"""
-    labLoan = 1
-    homeLoan = 2
+    lab_loan = 1
+    home_loan = 2
