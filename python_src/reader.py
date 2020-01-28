f46e9f7fc8b3520fc2cfe9bb44a239f702f3d4eb
Oleksandra Baga <oleksandra.baga@gmail.com>
2020-01-28 13:16:03 +0100

    readed card: no ATR shown

--- python_src/reader.py
+++ python_src/reader.py
@@ -1,10 +1,19 @@
 from smartcard.System import readers
 from smartcard.util import toHexString, toBytes
 from smartcard.ATR import ATR
-from smartcard.CardType import ATRCardType
+from smartcard.CardType import ATRCardType, AnyCardType
+
 from smartcard.CardRequest import CardRequest
-from .atr_cardtype import *
+from atr_cardtype import *
 
 
 rfid = readers()
 print(rfid)
+
+cardtype = AnyCardType()
+cardrequest = CardRequest( timeout=10, cardType=cardtype )
+cardservice = cardrequest.waitforcard()
+cardservice.connection.connect()
+
+detected_card = toHexString(cardservice.connection.getATR())
+print(cardservice.connection.getReader())
