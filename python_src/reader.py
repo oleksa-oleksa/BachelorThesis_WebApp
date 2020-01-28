01abd818dd1be6450bd78648806737dd6fa2f474
Oleksandra Baga <oleksandra.baga@gmail.com>
2020-01-28 12:40:44 +0100

    creating ATR for 3 types of cards presented in the project

--- python_src/reader.py
+++ python_src/reader.py
@@ -1,5 +1,9 @@
 from smartcard.System import readers
-from smartcard.util import toHexString
+from smartcard.util import toHexString, toBytes
+from smartcard.ATR import ATR
+from smartcard.CardType import ATRCardType
+from smartcard.CardRequest import CardRequest
+
 
 rfid = readers()
 print(rfid)
