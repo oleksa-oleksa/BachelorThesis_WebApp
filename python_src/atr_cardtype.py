2858fa8068dd6d3f69bf508f4619ab8b6c0d2af7
Oleksandra Baga <oleksandra.baga@gmail.com>
2020-01-28 13:16:03 +0100

    readed card: no ATR shown

--- python_src/atr_cardtype.py
+++ python_src/atr_cardtype.py
@@ -9,9 +9,11 @@ These interface bytes are then followed by Historical Bytes which are not standa
 nformation such as the card type, the version of the embedded software, or the card state. 
 Finally these historical bytes are eventually followed by a checksum byte.
 """
-from smartcard.ATR import ATR
+from smartcard.CardType import ATRCardType
+from smartcard.util import toBytes
 
-STUDENT_CARD = ATR([0x3B, 0x81, 0x80, 0x01, 0x80, 0x80])
-RASPI_TAG_SQUARE = ART([0x3B, 0x8F, 0x80, 0x01, 0x80, 0x4F, 0x0C, 0xA0, 0x00, 0x00, 0x03, 0x06, 0x03, 0x00, 0x03, 0x00, 0x00, 0x00, 0x00, 0x68])
-RASPI_TAG_ROUND = ART([0x3B, 0x8F, 0x80, 0x01, 0x80, 0x4F, 0x0C, 0xA0, 0x00, 0x00, 0x03, 0x06, 0x03, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x6A])
+
+STUDENT_CARD = ATRCardType(toBytes("3B 81 80 01 80 80"))
+RASPI_TAG_RECTANGLE = ATRCardType(toBytes("3B 8F 80 01 80 4F 0C A0 00 00 03 06 03 00 03 00 00 00 00 68"))
+RASPI_TAG_ROUND = ATRCardType(toBytes("3B 8F 80 01 80 4F 0C A0 00 00 03 06 03 00 01 00 00 00 00 6A"))
 
