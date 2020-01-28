5308944418510d35b6505cd23a4bc706933466fa
Oleksandra Baga <oleksandra.baga@gmail.com>
2020-01-28 14:34:14 +0100

    read is card:  ATR is shown

--- python_src/atr_cardtype.py
+++ python_src/atr_cardtype.py
@@ -12,9 +12,9 @@ Finally these historical bytes are eventually followed by a checksum byte.
 from smartcard.CardType import ATRCardType
 from smartcard.util import toBytes
 
-ATR_STUDENT_CARD = ATRCardType(toBytes("3B 81 80 01 80 80"))
-ATR_RASPI_TAG = ATRCardType(toBytes("3B 8F 80 01 80 4F 0C A0 00 00 03 06 03 00 01 00 00 00 00 6A"))
-ATR_RASPI_TAG_RECTANGLE = ATRCardType(toBytes("3B 8F 80 01 80 4F 0C A0 00 00 03 06 03 00 03 00 00 00 00 68"))
+ATR_STUDENT_CARD = "3B 81 80 01 80 80"
+ATR_RASPI_TAG = "3B 8F 80 01 80 4F 0C A0 00 00 03 06 03 00 01 00 00 00 00 6A"
+ATR_RASPI_TAG_RECTANGLE = "3B 8F 80 01 80 4F 0C A0 00 00 03 06 03 00 03 00 00 00 00 68"
 
-ATR_CARD_TYPES =
+ATR_CARD_TYPES = ATRCardType(toBytes(ATR_STUDENT_CARD), toBytes(ATR_RASPI_TAG))
 
