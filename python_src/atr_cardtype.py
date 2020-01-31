b08975124c422694e520c186c610c12a4ca16d31
Oleksandra Baga <oleksandra.baga@gmail.com>
2020-01-31 13:15:44 +0100

    reader is ok when card is not presented

--- python_src/atr_cardtype.py
+++ python_src/atr_cardtype.py
@@ -11,11 +11,24 @@ Finally these historical bytes are eventually followed by a checksum byte.
 """
 from smartcard.CardType import ATRCardType
 from smartcard.util import toBytes
+from smartcard.CardType import CardType
+
 
 ATR_STUDENT_CARD_HEX = "3B 81 80 01 80 80"
 ATR_RASPI_TAG_HEX = "3B 8F 80 01 80 4F 0C A0 00 00 03 06 03 00 01 00 00 00 00 6A"
 
 ATR_STUDENT_CARD = ATRCardType(toBytes(ATR_STUDENT_CARD_HEX))
 ATR_RASPI_TAG = ATRCardType(toBytes(ATR_RASPI_TAG_HEX))
+
+class StudentCard(CardType):
+	def init(self):
+		self.atr_hex = ATR_STUDENT_CARD_HEX
+		self.atr = ATR_STUDENT_CARD
+	
+		
+	def matches(self, atr, reader=None):
+		return atr[0] == 0x3B
+
+
 
 
