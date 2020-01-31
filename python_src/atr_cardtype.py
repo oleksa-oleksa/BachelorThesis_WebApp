b99ac143ab1d0b61f62496f251c3212119785194
Oleksandra Baga <oleksandra.baga@gmail.com>
2020-01-31 14:24:32 +0100

    reader is ok when card is not presented

--- python_src/atr_cardtype.py
+++ python_src/atr_cardtype.py
@@ -12,7 +12,8 @@ Finally these historical bytes are eventually followed by a checksum byte.
 from smartcard.CardType import ATRCardType
 from smartcard.util import toBytes
 from smartcard.CardType import CardType
-
+from student import *
+from board import *
 
 ATR_STUDENT_CARD_HEX = "3B 81 80 01 80 80"
 ATR_RASPI_TAG_HEX = "3B 8F 80 01 80 4F 0C A0 00 00 03 06 03 00 01 00 00 00 00 6A"
@@ -21,14 +22,33 @@ ATR_STUDENT_CARD = ATRCardType(toBytes(ATR_STUDENT_CARD_HEX))
 ATR_RASPI_TAG = ATRCardType(toBytes(ATR_RASPI_TAG_HEX))
 
 class StudentCard(CardType):
-	def init(self):
+	"""
+	Student card class
+	"""
+	def init(self, student=None):
 		self.atr_hex = ATR_STUDENT_CARD_HEX
 		self.atr = ATR_STUDENT_CARD
-	
+		self.student = student
 		
-	def matches(self, atr, reader=None):
-		return atr[0] == 0x3B
+	def matches(self, atr, reader=None): 
+	"""Returns true if atr and card connected match the CardType. 
+		
+	@param atr:    the atr to chek for matching 
+	@param reader: the reader (optional); default is None 
+	   
+	The reader can be use in some sub-classes to do advanced 
+	matching that require connecting to the card.""" 
+	return atr 
 
 
+class RaspiTag(CardType):
+	"""
+	Raspberry board RFID Tag
+	"""
+	def init(self, board=None):
+		self.atr_hex = ATR_RASPI_TAG_HEX
+		self.atr = ATR_RASPI_TAG
+		self.board = board
+	
 
 
