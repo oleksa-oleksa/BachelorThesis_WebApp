b1d9072f7c162050191955b198ece879bd0595f2
Oleksandra Baga <oleksandra.baga@gmail.com>
2020-02-04 15:10:42 +0100

    added APDU for Mifare DesFire

--- python_src/mifare_command_set.py
+++ python_src/mifare_command_set.py
@@ -1,5 +1,12 @@
 from smartcard.util import toHexString, toBytes
 
+"""
+A step in an application protocol consists of sending a command, processing it in the receiving entity and sending back the response. 
+Therefore a spcecific response corresponds to a specific command, referred to as a command-response pair.
+An application protocol data unit (APDU) contains either a command message or a response message, sent from the interface device to the card or conversely.
+In a command-response pair, the command message and the response message may contain data, thus inducing four cases 
+"""
+
 
 INS_SELECT = 0xA4
 INS_READ_BINARY = 0xB0
