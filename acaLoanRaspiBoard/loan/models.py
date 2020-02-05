68e9ad4cd7612e2ac8d553ace8348bd1dfb4fdea
Oleksandra Baga <oleksandra.baga@gmail.com>
2020-02-05 17:57:02 +0100

    started models. todo: Board model

--- acaLoanRaspiBoard/loan/models.py
+++ acaLoanRaspiBoard/loan/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from python_src import board_type, board_status
 
-# Create your models here.
+
+class Student(models.Model):
+	studentcard_uid = models.CharField(max_length=50, primary_key=True)
+	first_name = models.CharField('first name', max_length=50)
+	second_name = models.CharField('second name', max_length=50)
+	matricul_no = models.CharField('matriculation', max_length=10, unique=True)
+	hrz_no = models.CharField('hrz', max_length=10, unique=True)
+	group = models.CharField(max_length=1)
+	is_home_loan_enabled = models.BooleanField(default=True)
+	list_of_board = list_of_boards
+ 
+
+class Board(models.Model):
+	board_uid = models.CharField(max_length=50, primary_key=True)
+	board_no = models.CharField('matriculation', max_length=10, unique=True)
+	board_type = models.CharField(max_length=10, types=[(t_board, t_board.value) for t_board in BoardType]  # types is a list of Tuple
+	board_status = models.CharField(max_length=10, states=[(t_status, t_status.value) for t_status in BoardStatus]  # types is a list of Tuple
+	is_board_loaned = models.BooleanField(default=False)
+	loan_date = loan_date
+	student = student
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
---
+++ acaLoanRaspiBoard/python_src/rfig_tag.py
@@ -0,0 +1,5 @@
+class RFIDTag:
+    """
+    Represents the RFID Tag information of each RFID Tag known to the system
+    """
+    pass
