dfa9ab9bfe192a7e11abd06b8513671cb469835d
Oleksandra Baga <oleksandra.baga@gmail.com>
2020-02-12 13:12:25 +0100

    adding loanedBoards table

--- acaLoanRaspiBoard/python_src/student.py
+++ acaLoanRaspiBoard/python_src/student.py
@@ -6,11 +6,11 @@ class Student:
     home_loan_enabled flag is to set by the admin in case the board was returned damaged
     and a student has not notified the teaching assistance about what happened
     """
-    def __init__(self, studentcard_uid=None, first_name='',
+    def __init__(self, studentcard=None, first_name='',
                  second_name='', matricul_no='', hrz_no='',
                  group=None, is_home_loan_enabled=True):
         """Initialises a new student with given parameters"""
-        self.studentcard_uid = studentcard_uid
+        self.studentcard = studentcard
         self.first_name = first_name
         self.second_name = second_name
         self.matricul_no = matricul_no
