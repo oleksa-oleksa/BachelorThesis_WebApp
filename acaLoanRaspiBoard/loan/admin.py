d9af8c4af1acc9e0c3cba733475fb3f6297efc43
Oleksandra Baga <oleksandra.baga@gmail.com>
2020-02-17 15:38:38 +0100

    model creation SemesterList error

--- acaLoanRaspiBoard/loan/admin.py
+++ acaLoanRaspiBoard/loan/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Student, Board, Action, SemesterList, StudentCard, RaspiTag
+
 # Register your models here.
+admin.site.register(Student)
+admin.site.register(Board)
+admin.site.register(Action)
+admin.site.register(SemesterList)
+admin.site.register(StudentCard)
+admin.site.register(RaspiTag)
