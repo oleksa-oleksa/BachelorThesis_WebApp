33e480afbcc636c6c157afb70682f1082deda3b3
Oleksandra Baga <oleksandra.baga@gmail.com>
2020-03-02 15:13:12 +0100

    student model displayed as a table in django admin

--- acaLoanRaspiBoard/loan/admin.py
+++ acaLoanRaspiBoard/loan/admin.py
@@ -2,10 +2,18 @@ from django.contrib import admin
 
 from .models import Student, Board, Action, StudentCard, RaspiTag, Semester
 
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('student_card', 'semester', 'first_name', 'second_name', 'matricul_no', 'hrz_no',
+                    'group', 'is_home_loan_enabled')
+
+
 # Register your models here.
-admin.site.register(Student)
+admin.site.register(Student, StudentAdmin)
 admin.site.register(Board)
 admin.site.register(Action)
 admin.site.register(StudentCard)
 admin.site.register(RaspiTag)
 admin.site.register(Semester)
+
+
