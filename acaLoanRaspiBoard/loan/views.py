8e6517bc607639b937490aefe6fc8a7f3906a2d7
Oleksandra Baga <oleksandra.baga@gmail.com>
2020-03-18 13:35:19 +0100

    forms.py started

--- acaLoanRaspiBoard/loan/views.py
+++ acaLoanRaspiBoard/loan/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
 
 from .models import StudentGroup, Student, Operation, Board, Action
 
@@ -14,3 +16,20 @@ def index(request):
 	template_name = "loan/index.html"
 	context = {"home_boards_list": queryset}
 	return render(request, template_name, context)
+
+
+@staff_member_required
+def file_upload_view(request):
+	# create objects -> use a form
+	form = BlogPostModelForm(request.POST or None, request.FILES or None)
+	if form.is_valid():
+        # obj = BlogPost.objects.create(**form.cleaned_data)
+        obj = form.save(commit=False)
+        # obj.title = form.cleaned_data.get("title")
+        obj.user = request.user
+        obj.save()
+        form = BlogPostModelForm()
+    template_name = "blog/form.html"
+    context = {"form": form}
+    return render(request, template_name, context)
+
