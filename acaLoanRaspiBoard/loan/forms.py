919492b548fa3889ca9f0714351cb41eb1ec53ce
Oleksandra Baga <oleksandra.baga@gmail.com>
2020-03-19 15:42:05 +0100

    adding csv file, rfid_upload.html started

--- acaLoanRaspiBoard/loan/forms.py
+++ acaLoanRaspiBoard/loan/forms.py
@@ -3,23 +3,22 @@ from django_enumfield import enum
 from .models import ATRCardType, StudentCard, RaspiTag, Student, Board
 
 
-class StudentCardForm(forms.Form):
-    atr_hex = enum.EnumField(ATRCardType, default=ATRCardType.STUDENT_CARD_ATR)
-    uid = forms.CharField('Card UID', max_length=66, unique=True)
+class RaspiTagForm(forms.Form):
+    atr_hex = enum.EnumField(ATRCardType, default=ATRCardType.RASPI_TAG_ATR)
+    uid = forms.CharField()
 
 
-
-class StudentCardFormModel(forms.ModelForm):
+class RaspiTagFormModel(forms.ModelForm):
     # to change the form instead of model
     # title = forms.CharField(max_length=150)
     class Meta:
-        model = StudentCard
-        fields = ["title", "image", "slug", "content"]
+        model = RaspiTag
+        fields = ["atr_hex", "uid"]
 
     def clean_title(self, *args, **kwargs):
         instance = self.instance
         title = self.cleaned_data.get('title')
-        qs = BlogPost.objects.filter(title__iexact=title)
+        qs = RaspiTag.objects.filter(title__iexact=title)
         if instance is not None:
             qs = qs.exclude(pk=instance.pk)  # id=instance.id
         if qs.exists():
