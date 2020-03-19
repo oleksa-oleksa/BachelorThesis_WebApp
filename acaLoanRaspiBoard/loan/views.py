bcb8ab277b789b9a86277e287b77d19945e68d43
Oleksandra Baga <oleksandra.baga@gmail.com>
2020-03-19 15:42:05 +0100

    adding csv file, rfid_upload.html started

--- acaLoanRaspiBoard/loan/views.py
+++ acaLoanRaspiBoard/loan/views.py
@@ -3,9 +3,10 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib import messages
 
-from .models import StudentGroup, Student, Operation, Board, Action
-from .forms import StudentCardFormModel
+from .models import StudentGroup, Student, Operation, Board, Action, RaspiTag
+from .forms import RaspiTagFormModel
 
 """
 def index(request):
@@ -31,13 +32,28 @@ def rfid_upload(request):
 	if request.method == "GET":
 		return render(request, template_name, prompt)
 
+	csv_file = request.FILES['file']
+
+	if not csv_file.name.endswith('.csv'):
+		messages.error(request, "This is not a csv file!")
+
+	data_set = csv_file.read().decode('UTF-8')
+	io_string = io.StringIO(data_set)
+	next(io_string)
+
+	for row in csv.reader(io_string, delimiter=','):
+		_, createdTag = RaspiTag.objects.update_or_create(
+			uid=row[1]
+		)
+
+
 def file_upload_view(request):
 	# create objects -> use a form
-	form = StudentCardFormModel(request.POST or None, request.FILES or None)
+	form = RaspiTagFormModel(request.POST or None, request.FILES or None)
 	if form.is_valid():
 		obj = form.save(commit=False)
 		obj.save()
-		form = StudentCardFormModel()
+		form = RaspiTagFormModel()
 	template_name = "loan/file_upload.html"
 	context = {"form": form}
 	return render(request, template_name, context)
