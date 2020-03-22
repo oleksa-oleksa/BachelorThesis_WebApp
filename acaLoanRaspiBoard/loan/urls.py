994bd65b5ca6e8ae5a3e3616ba8f5073581f5f0f
Oleksandra Baga <oleksandra.baga@gmail.com>
2020-03-22 14:37:36 +0100

    <form>

--- acaLoanRaspiBoard/loan/urls.py
+++ acaLoanRaspiBoard/loan/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.contrib import admin
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('admin/', admin.site.urls),
+    path('upload_csv_rfid', views.)
 ]
