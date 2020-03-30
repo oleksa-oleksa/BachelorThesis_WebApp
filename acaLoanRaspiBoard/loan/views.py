2ea91dd8ea3a5e5e40bde7a6cf584c2f93ad6968
Oleksandra Baga <oleksandra.baga@gmail.com>
2020-03-30 17:18:35 +0200

    upload_student view and html

--- acaLoanRaspiBoard/loan/views.py
+++ acaLoanRaspiBoard/loan/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib import messages
 
-from .models import StudentGroup, Student, Operation, Board, Action, RaspiTag, ATRCardType
+from .models import StudentCard, Student, Operation, Board, Action, RaspiTag, ATRCardType
 from .forms import RaspiTagFormModel
 
 """
@@ -130,44 +130,52 @@ def upload_student(request):
 	# admin_get_boards.py creates csv-file without header (there is no need, file contains only two columns)
 	# next(io_string)
 
-	rfids_dict = {}
-	boards_failed_dict = {}
-	rfids_uids_failed_dict = {}
+	cards_dict = {}
+	student_failed_dict = {}
+	cards_uids_failed_dict = {}
 	counter = 0
 	for row in csv.reader(io_string, delimiter=','):
-		created_tag, _ = RaspiTag.objects.update_or_create(
-			atr_hex=ATRCardType.RASPI_TAG_ATR,
+		created_card, _ = StudentCard.objects.update_or_create(
+			atr_hex=ATRCardType.STUDENT_CARD_ATR,
 			uid=row[1]
 		)
 		"""The update() method adds element(s) to the dictionary if the key is not in the dictionary. 
 		If the key is in the dictionary, it updates the key with the new value."""
 
 		# board_no is duplicated
-		if row[0] in rfids_dict.keys():
-			if row[0] not in boards_failed_dict.keys():
-				boards_failed_dict[row[0]] = [row[1]]
+		if row[0] in cards_dict.keys():
+			if row[0] not in student_failed_dict.keys():
+				student_failed_dict[row[0]] = [row[1]]
 			else:
-				boards_failed_dict[row[0]].append(row[1])
+				student_failed_dict[row[0]].append(row[1])
 
 		# rfid_tag is duplicated
-		if row[1] in rfids_dict.values():
-			if row[1] not in rfids_uids_failed_dict.keys():
-				rfids_uids_failed_dict[row[1]] = [row[0]]
+		if row[1] in cards_dict.values():
+			if row[1] not in cards_uids_failed_dict.keys():
+				cards_uids_failed_dict[row[1]] = [row[0]]
 			else:
-				rfids_uids_failed_dict[row[1]].append(row[0])
+				cards_uids_failed_dict[row[1]].append(row[0])
 		# everything is okay
-		if row[0] not in rfids_dict.keys() and row[1] not in rfids_dict.values():
-			rfids_dict.update({row[0]: row[1]})
-			Board.objects.filter(board_no=row[0]).update(raspi_tag=created_tag)
+		if row[0] not in cards_dict.keys() and row[1] not in cards_dict.values():
+			cards_dict.update({row[0]: row[1]})
+			Student.objects.filter(matricul_no=row[0]).update(student_card=created_card)
 
 		counter += 1
 
 	# select * from raspi_tag order by id desc
 	# seelct * from raspi_tag join boards on raspitag.id = boards.rapbitag_id
-	queryset = RaspiTag.objects.select_related('board').all().order_by('-id')[:counter]
+	queryset = StudentCard.objects.select_related('student').all().order_by('-id')[:counter]
 
 	context = {"csv_uploaded": "True", "boards_uid_list": queryset, "counter": counter,
-				"rfids_dict": rfids_dict, "boards_failed_dict": boards_failed_dict,
-				"rfids_uids_failed_dict": rfids_uids_failed_dict}
+				"cards_dict": cards_dict, "student_failed_dict": student_failed_dict,
+				"cards_uids_failed_dict": cards_uids_failed_dict}
 	return render(request, template_name_submitted, context)
 
+
+@staff_member_required
+def link_students(request):
+	#queryset = RaspiTag.objects.all()
+	template_name = "loan/link_students.html"
+	#context = {"boards_uid_list": queryset}
+	context = {}
+	return render(request, template_name, context)
