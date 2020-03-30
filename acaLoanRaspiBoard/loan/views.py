6f9fe220219111c5ec7ad35e779f0e22e250934b
Oleksandra Baga <oleksandra.baga@gmail.com>
2020-03-30 17:06:40 +0200

    upload_student view and html

--- acaLoanRaspiBoard/loan/views.py
+++ acaLoanRaspiBoard/loan/views.py
@@ -92,6 +92,7 @@ def upload_rfid(request):
 				"rfids_uids_failed_dict": rfids_uids_failed_dict}
 	return render(request, template_name_submitted, context)
 
+
 @staff_member_required
 def link_boards(request):
 	#queryset = RaspiTag.objects.all()
@@ -99,3 +100,74 @@ def link_boards(request):
 	#context = {"boards_uid_list": queryset}
 	context = {}
 	return render(request, template_name, context)
+
+
+@staff_member_required
+def upload_student(request):
+	template_name = "loan/upload_student.html"
+	template_name_submitted = "loan/link_student.html"
+
+
+	prompt = {
+		'order': 'Order of CSV should be: matricul_no,  card_uid'
+	}
+
+	if request.method == "GET":
+		return render(request, template_name, prompt)
+
+	csv_file = request.FILES['student_list']
+
+	if not csv_file.name.endswith('.csv'):
+		messages.error(request, "This is not a csv file!")
+
+	if csv_file.multiple_chunks():
+		messages.error(request, "Uploaded file is too big (%.2f MB)." % (csv_file.size / (1000 * 1000),))
+		return render(request, template_name, prompt)
+
+	data_set = csv_file.read().decode('UTF-8')
+	"""In-memory text streams are also available as StringIO objects:"""
+	io_string = io.StringIO(data_set)
+	# admin_get_boards.py creates csv-file without header (there is no need, file contains only two columns)
+	# next(io_string)
+
+	rfids_dict = {}
+	boards_failed_dict = {}
+	rfids_uids_failed_dict = {}
+	counter = 0
+	for row in csv.reader(io_string, delimiter=','):
+		created_tag, _ = RaspiTag.objects.update_or_create(
+			atr_hex=ATRCardType.RASPI_TAG_ATR,
+			uid=row[1]
+		)
+		"""The update() method adds element(s) to the dictionary if the key is not in the dictionary. 
+		If the key is in the dictionary, it updates the key with the new value."""
+
+		# board_no is duplicated
+		if row[0] in rfids_dict.keys():
+			if row[0] not in boards_failed_dict.keys():
+				boards_failed_dict[row[0]] = [row[1]]
+			else:
+				boards_failed_dict[row[0]].append(row[1])
+
+		# rfid_tag is duplicated
+		if row[1] in rfids_dict.values():
+			if row[1] not in rfids_uids_failed_dict.keys():
+				rfids_uids_failed_dict[row[1]] = [row[0]]
+			else:
+				rfids_uids_failed_dict[row[1]].append(row[0])
+		# everything is okay
+		if row[0] not in rfids_dict.keys() and row[1] not in rfids_dict.values():
+			rfids_dict.update({row[0]: row[1]})
+			Board.objects.filter(board_no=row[0]).update(raspi_tag=created_tag)
+
+		counter += 1
+
+	# select * from raspi_tag order by id desc
+	# seelct * from raspi_tag join boards on raspitag.id = boards.rapbitag_id
+	queryset = RaspiTag.objects.select_related('board').all().order_by('-id')[:counter]
+
+	context = {"csv_uploaded": "True", "boards_uid_list": queryset, "counter": counter,
+				"rfids_dict": rfids_dict, "boards_failed_dict": boards_failed_dict,
+				"rfids_uids_failed_dict": rfids_uids_failed_dict}
+	return render(request, template_name_submitted, context)
+
