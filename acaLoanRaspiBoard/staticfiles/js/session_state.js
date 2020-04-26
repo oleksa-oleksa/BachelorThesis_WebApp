83485d173b6656ea5873e1f762aff0c62a0f2974
Oleksandra Baga <oleksandra.baga@gmail.com>
2020-04-26 14:56:49 +0200

    added clearInterval if session_id was not found or timeout was reached

--- acaLoanRaspiBoard/staticfiles/js/session_state.js
+++ acaLoanRaspiBoard/staticfiles/js/session_state.js
@@ -1,13 +1,20 @@
+var session_refresh_timer
+
 function refresh_session_state() {
     $.ajax(
-    {url: "api/sessions/16", method: "GET"}).done(function(body) {
+    {url: "api/sessions/14", method: "GET"}).done(function(body) {
         console.log(body);
     })
+    .fail(function() {
+        clearInterval(session_refresh_timer)
+        alert("Something is wrong. Timeout. Please start again!")
+        window.location.replace("/loan")
+        })
 
 }
 
 $(document).ready(function() {
     console.log("ready!");
-    setInterval(refresh_session_state, 1000)
+    session_refresh_timer = setInterval(refresh_session_state, 1000)
 })
 
