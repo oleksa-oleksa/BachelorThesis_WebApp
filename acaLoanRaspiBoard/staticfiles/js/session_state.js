d59139d0ad4bc5015fd4a0360219ba16b3767ff4
Oleksandra Baga <oleksandra.baga@gmail.com>
2020-04-26 14:34:45 +0200

    used jquery to request session state asynchronously

--- acaLoanRaspiBoard/staticfiles/js/session_state.js
+++ acaLoanRaspiBoard/staticfiles/js/session_state.js
@@ -1,9 +1,13 @@
 function refresh_session_state() {
-    console.log( "ready!" );
+    $.ajax(
+    {url: "api/sessions/16", method: "GET"}).done(function(body) {
+        console.log(body);
+    })
 
 }
 
 $(document).ready(function() {
+    console.log("ready!");
     setInterval(refresh_session_state, 1000)
 })
 
