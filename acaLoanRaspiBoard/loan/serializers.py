10f600554d19f0bd6cb5e3399815161641dc2409
Oleksandra Baga <oleksandra.baga@gmail.com>
2020-06-06 14:46:43 +0200

    read student card and assigned boards from action model

--- acaLoanRaspiBoard/loan/serializers.py
+++ acaLoanRaspiBoard/loan/serializers.py
@@ -8,10 +8,20 @@ def render_session(session):
         session_student = student_first_name + " " + student_second_name
 
         boards = student.get_student_boards()
-        loaned_board = boards["lab"].board_no + " " + boards["lab"].board_status
+
+        if boards["lab"] == "":
+            loaned_lab_board = "No lab boards assigned"
+        else:
+            loaned_lab_board = boards["lab"].board_no + " " + boards["lab"].board_status
+
+        if boards["home"] == "":
+            loaned_home_board = boards["home"].board_no + " " + boards["home"].board_status
+        else:
+            loaned_home_board = "No home loan board assigned"
     else:
         session_student = ""
-        loaned_board = ""
+        loaned_lab_board = ""
+        loaned_home_board = ""
 
     scanned_board = session.get_active_board()
     if scanned_board is not None:
@@ -20,5 +30,5 @@ def render_session(session):
         session_board = "Please scan board"
 
     session_dict = {"state": session_state, "student": session_student, "scanned_board": session_board,
-                    "loaned_board": loaned_board}
+                    "loaned_lab_board": loaned_lab_board, "loaned_home_board": loaned_home_board}
     return session_dict
