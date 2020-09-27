77df919f2ad7bc6ab111c94dc0c6430cd1faa41b
Oleksandra Baga <oleksandra.baga@gmail.com>
2020-09-27 15:12:47 +0200

    test homeloan board return in progress

--- acaLoanRaspiBoard/loan/tests.py
+++ acaLoanRaspiBoard/loan/tests.py
@@ -1,9 +1,22 @@
 from django.test import TestCase
-from .models import StudentCard, Student, RaspiTag, Board, ATRCardType, Semester, StudentGroup, BoardType, BoardStatus, Session
+from .models import StudentCard, Student, RaspiTag, Board, ATRCardType, Semester, StudentGroup, BoardType, BoardStatus
+from .models import Session, Action, Operation
 from . import factories
 
 
 # Create your tests here.
+
+class TestLabLoanBard(TestCase):
+    def setUp(self):
+        semester = Semester.objects.create(semester="WS20/21")
+        self.student_home_enabled = factories.StudentFactory(semester=semester)
+        self.student_home_disabled = factories.StudentFactory(semester=semester, is_home_loan_enabled=False)
+        self.lab_board_active = factories.BoardLabFactory(board_no=3)
+        self.lab_board_loaned = factories.BoardLabFactory(board_no=4, board_status=BoardStatus.LOANED)
+        self.home_board_active = factories.BoardHomeFactory(board_no=13)
+        self.home_board_loaned = factories.BoardHomeFactory(board_no=14, board_status=BoardStatus.LOANED)
+
+
 class TestHomeLoanBoard(TestCase):
     def setUp(self):
         semester = Semester.objects.create(semester="WS20/21")
@@ -24,3 +37,11 @@ class TestHomeLoanBoard(TestCase):
                                          raspi_tag=self.home_board_active.raspi_tag)
         self.assertEqual(session.board_loaned(), 'home_loan_disabled')
 
+    def test_home_return(self):
+        session = Session.objects.create(state='rfid_state_loaned', student_card=self.student_home_enabled.student_card,
+                                         raspi_tag=self.home_board_active.raspi_tag)
+        #action_loan = Action.loan_board_action(student=self.student_home_enabled, board=self.home_board_active,
+                                               operation=Operation.HOME_LOAN)
+        #action_loan.save()
+
+        self.assertEqual(session.board_returned(), 'returned')
