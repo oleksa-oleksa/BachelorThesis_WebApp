bda6732b60f3e599251b31c5c6618dc857900fe7
Oleksandra Baga <oleksandra.baga@gmail.com>
2020-09-28 14:31:49 +0200

    tested error states for homeLoan board

--- acaLoanRaspiBoard/loan/tests.py
+++ acaLoanRaspiBoard/loan/tests.py
@@ -21,6 +21,7 @@ class TestHomeLoanBoard(TestCase):
     def setUp(self):
         semester = Semester.objects.create(semester="WS20/21")
         self.student_home_enabled = factories.StudentFactory(semester=semester)
+        self.student_home_enabled_second = factories.StudentFactory(semester=semester)
         self.student_home_disabled = factories.StudentFactory(semester=semester, is_home_loan_enabled=False)
         self.lab_board_active = factories.BoardLabFactory(board_no=3)
         self.lab_board_loaned = factories.BoardLabFactory(board_no=4, board_status=BoardStatus.LOANED)
@@ -39,9 +40,50 @@ class TestHomeLoanBoard(TestCase):
 
     def test_home_return(self):
         session = Session.objects.create(state='rfid_state_loaned', student_card=self.student_home_enabled.student_card,
-                                         raspi_tag=self.home_board_active.raspi_tag)
-        #action_loan = Action.loan_board_action(student=self.student_home_enabled, board=self.home_board_active,
-                                               operation=Operation.HOME_LOAN)
-        #action_loan.save()
+                                         raspi_tag=self.home_board_loaned.raspi_tag)
+        Action.loan_board_action(student=self.student_home_enabled, board=self.home_board_active,
+                                 operation=Operation.HOME_LOAN)
+        self.home_board_active.loan_board(self.home_board_active.raspi_tag)
+        self.assertEqual(session.loaned_board_returned(), 'returned')
 
-        self.assertEqual(session.board_returned(), 'returned')
+    def test_loan_second_board_same_type(self):
+        # loan first board
+        Action.loan_board_action(student=self.student_home_enabled, board=self.home_board_loaned,
+                                 operation=Operation.HOME_LOAN)
+
+        session = Session.objects.create(state='rfid_state_active', student_card=self.student_home_enabled.student_card,
+                                         raspi_tag=self.home_board_active.raspi_tag)
+        self.assertEqual(session.board_loaned(), 'same_bord_type')
+
+    def test_loan_same_board_twice(self):
+        # loan first time
+        Action.loan_board_action(student=self.student_home_enabled, board=self.home_board_loaned,
+                                 operation=Operation.HOME_LOAN)
+
+        session = Session.objects.create(state='rfid_state_active', student_card=self.student_home_enabled.student_card,
+                                         raspi_tag=self.home_board_loaned.raspi_tag)
+        # board_status != BoardStatus.ACTIVE
+        self.assertEqual(session.board_loaned(), 'error')
+
+        # change board status to be active again but no record about return operation is exist
+        self.home_board_loaned.board_status = BoardStatus.ACTIVE
+        self.assertEqual(session.board_loaned(), 'same_bord_type')
+
+    def test_home_return_unassigned_board(self):
+        # loan board to a first student
+        Action.loan_board_action(student=self.student_home_enabled, board=self.home_board_loaned,
+                                 operation=Operation.HOME_LOAN)
+
+        # create a session for a second student and scan a previous loaned board
+        session = Session.objects.create(state='rfid_state_loaned', student_card=self.student_home_enabled_second.student_card,
+                                         raspi_tag=self.home_board_loaned.raspi_tag)
+
+        self.assertEqual(session.loaned_board_returned(), 'return_error')
+
+    def test_home_loan_third_board(self):
+        pass
+
+    def test_home_loan_non_existing_board(self):
+        pass
+
+
