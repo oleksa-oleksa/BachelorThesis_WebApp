4b089508594781862f5bf2db4be2dabcc866ec2b
Oleksandra Baga <oleksandra.baga@gmail.com>
2022-02-02 11:11:23 +0100

    Folders renamed: acaLoan_Server_API contains Django project and display client for interactive visual updates, REST API. acaLoan_RegisterClient contains python source files that have to be executed on Raspberry Pi with connected RFID Reader.

--- acaLoan_Server_API/acaLoan_Server_API/settings.py
+++ acaLoan_Server_API/acaLoan_Server_API/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'acaLoanRaspiBoard.urls'
+ROOT_URLCONF = 'acaLoan_Server_API.urls'
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-ASGI_APPLICATION = 'acaLoanRaspiBoard.asgi.application'
+ASGI_APPLICATION = 'acaLoan_Server_API.asgi.application'
 
 
 # Database
--- acaLoan_Server_API/manage.py
+++ acaLoan_Server_API/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'acaLoanRaspiBoard.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'acaLoan_Server_API.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
